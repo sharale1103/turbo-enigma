85d6877ae6614ca59d33ccb3ff907fff8df95429
Sarthak <97030182+sharale1103@users.noreply.github.com>
2025-03-08 10:25:30 +0000

    login done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const connectDB = async () => {
   try {
 
-    const conn = await mongoose.connect("mongodb+srv://sharale1103:ii.2002.ii@cluster0.c2wyu.mongodb.net/chat-db?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log('connected ${conn.connection.host}')
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,7 +6,7 @@ import{login,logout,signup} from "../controllers/auth.controller.js"
 router.post("/signup", signup);
 
 
-router.get("/login",login);
+router.post("/login",login);
 
 
 router.post("/logout", logout);
